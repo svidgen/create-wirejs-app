914a7612b79280f5a29b9ac79fdcc56ae44aea0a
Jon Wire <iambipedal@gmail.com>
2025-01-28 17:01:32 -0600

    fix typo ...

--- packages/create-wirejs-deploy-amplify-basic/bin.js
+++ packages/create-wirejs-deploy-amplify-basic/bin.js
@@ -15,7 +15,7 @@ Amplify configuration written. Your next steps:
   git commit -m "added amplify deployment config"
   git push
 
-2. Visit hhttps://us-east-2.console.aws.amazon.com/amplify/create/add-repo
+2. Visit https://us-east-2.console.aws.amazon.com/amplify/create/add-repo
 3. Add your repository info and continue the setup.
 
 After setup, Amplify will act as your CI/CD platform and deploy your app.
